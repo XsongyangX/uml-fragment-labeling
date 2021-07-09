2f5b0c3685f516995a3e411f623d21755fd86ab2
Song Yang <songyang1218@gmail.com>
2021-07-08 20:16:39 -0400

    Basic index layout

--- umllabels/views.py
+++ umllabels/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 # Home index
 
 def index(request):
-    return render(request, "umllabels/index.html")
+    context = {
+        "total_models": 289,
+        "total_fragments": 8215,
+        "models_done": 0,
+        "fragments_done": 0
+    }
+    return render(request, "umllabels/index.html", context=context)
