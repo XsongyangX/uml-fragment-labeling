f90e789c8f5b159a8872f5e82316fa0df8e0c0ce
Song Yang <songyang1218@gmail.com>
2021-07-15 17:36:06 -0400

    Refactor image viewers

--- labeling/static/umllabels/drag.js
+++ labeling/static/umllabels/drag.js
@@ -2,7 +2,7 @@
 
 var _DRAGGGING_STARTED = 0;
 var _LAST_MOUSEMOVE_POSITION = { x: null, y: null };
-var _DIV_OFFSET = $('#image-container').offset();
+var _DIV_OFFSET = $('.image-container').offset();
 var _CONTAINER_WIDTH;
 var _CONTAINER_HEIGHT;
 var _IMAGE_WIDTH;
@@ -11,27 +11,27 @@ var _IMAGE_LOADED = 0;
 
 // Check whether image is cached or wait for the image to load 
 // This is necessary before calculating width and height of the image
-if ($('#drag-image').get(0).complete) {
+if ($('.drag-image').get(0).complete) {
     ImageLoaded();
 }
 else {
-    $('#drag-image').on('load', function () {
+    $('.drag-image').on('load', function () {
         ImageLoaded();
     });
 }
 
 // Image is loaded
 function ImageLoaded() {
-    _IMAGE_WIDTH = $("#drag-image").width();
-    _IMAGE_HEIGHT = $("#drag-image").height();
+    _IMAGE_WIDTH = $(".drag-image").width();
+    _IMAGE_HEIGHT = $(".drag-image").height();
 
-    _CONTAINER_WIDTH = $("#image-container").outerWidth();
-    _CONTAINER_HEIGHT = $("#image-container").outerHeight();
+    _CONTAINER_WIDTH = $(".image-container").outerWidth();
+    _CONTAINER_HEIGHT = $(".image-container").outerHeight();
 
     _IMAGE_LOADED = 1;
 }
 
-$('#image-container').on('mousedown', function (event) {
+$('.image-container').on('mousedown', function (event) {
     /* Image should be loaded before it can be dragged */
     if (_IMAGE_LOADED == 1) {
         _DRAGGGING_STARTED = 1;
@@ -41,11 +41,11 @@ $('#image-container').on('mousedown', function (event) {
     }
 });
 
-$('#image-container').on('mouseup', function () {
+$('.image-container').on('mouseup', function () {
     _DRAGGGING_STARTED = 0;
 });
 
-$('#image-container').on('mousemove', function (event) {
+$('.image-container').on('mousemove', function (event) {
     if (_DRAGGGING_STARTED == 1) {
         var current_mouse_position = { x: event.pageX - _DIV_OFFSET.left, y: event.pageY - _DIV_OFFSET.top };
         var change_x = current_mouse_position.x - _LAST_MOUSE_POSITION.x;
@@ -54,8 +54,10 @@ $('#image-container').on('mousemove', function (event) {
         /* Save mouse position */
         _LAST_MOUSE_POSITION = current_mouse_position;
 
-        var img_top = parseInt($("#drag-image").css('top'), 10);
-        var img_left = parseInt($("#drag-image").css('left'), 10);
+        var thisImage = $(this).children(".drag-image").first();
+
+        var img_top = parseInt(thisImage.css('top'), 10);
+        var img_left = parseInt(thisImage.css('left'), 10);
 
         var img_top_new = img_top + change_y;
         var img_left_new = img_left + change_x;
@@ -85,16 +87,16 @@ $('#image-container').on('mousemove', function (event) {
 
         // perform change component wise
         if (leftSide || rightSide)
-            $("#drag-image").css({ left: img_left_new + 'px' });
+            thisImage.css({ left: img_left_new + 'px' });
         if (topSide || botSide)
-            $("#drag-image").css({ top: img_top_new + 'px' })
+            thisImage.css({ top: img_top_new + 'px' })
     }
 });
 
 // https://stackoverflow.com/a/42058518/12944664
 var zoomSpeed = 0.1;
 var currentScale = 1;
-$("#image-container").on('wheel', function (event) {
+$(".image-container").on('wheel', function (event) {
     event.preventDefault(); // https://stackoverflow.com/a/7600806/12944664
 
     // deltaY obviously records vertical scroll, deltaX and deltaZ exist too.
@@ -114,10 +116,11 @@ $("#image-container").on('wheel', function (event) {
         // greater than 10%
         currentScale = currentScale <= 0.1 ? 0.1 : currentScale;
 
-        $("#drag-image").css("transform", "scale(" + currentScale + ")");
+        $(this).children(".drag-image").css("transform", "scale(" + currentScale + ")");
     }
 });
 
-$("#re-center-button").click(function(event) {
-    $("#drag-image").css({transform: "none", left: "0px", top: "0px"});
+$(".re-center-button").click(function(event) {
+    // button shares a common parent with the image container
+    $(this).parent().find(".drag-image").css({transform: "none", left: "0px", top: "0px"});
 });
