44db1b8268dd30ce9cdf5b34418efdd114bcb3f7
Song Yang <songyang1218@gmail.com>
2021-07-27 23:46:25 -0400

    Logging and build

---
+++ heroku.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# To run after each push on Heroku
+
+# 1. Download all fragments from github
+bash download.sh
+
+# 2. Migrate
+python manage.py migrate
+
+# 3. Delete extrenous data
+python manage.py shell -c 'from django.contrib.contenttypes.models import ContentType;ContentType.objects.all().delete()'
+
+# 4. Load data from json
+python manage.py loaddata newfile.json
+
+# 5. Collect fragments from github
+python manage.py collectstatic
