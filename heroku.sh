3aa59d06002691dca2437b3989103029fb6ceb17
Song Yang <songyang1218@gmail.com>
2021-07-28 00:56:50 -0400

    Dump name

--- heroku.sh
+++ heroku.sh
@@ -12,7 +12,7 @@ python manage.py migrate
 python manage.py shell -c 'from django.contrib.contenttypes.models import ContentType;ContentType.objects.all().delete()'
 
 # 4. Load data from json
-python manage.py loaddata newfile.json
+python manage.py loaddata dump_fragments.json
 
 # 5. Collect fragments from github
 python manage.py collectstatic
