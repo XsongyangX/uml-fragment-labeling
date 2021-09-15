1f1d241a21a21be67fe28f0cebb978f44ca2d346
Song Yang <songyang1218@gmail.com>
2021-09-14 22:05:37 -0400

    Image in admin

--- labeling/admin.py
+++ labeling/admin.py
@@ -6,7 +6,16 @@ from django.utils.html import format_html
 # Register your models here.
 @admin.register(Label)
 class LabelsAdmin(admin.ModelAdmin):
-    fields = ("label", "in_english", "validated", )
+    fields = ("label", "in_english", "validated", "display_image")
+    readonly_fields = ['display_image']
+    
+    def display_image(self, obj):
+        # get image url 
+        image_url = f"/static/fragments/{obj}.png"
+        if image_url is not None:
+            return format_html('<img src="{}">', image_url)
+        return None
+
 
     # def image(self, obj):
     #     return format_html('<img src="{0}" />'.format(f"/static/fragments/{obj}.png"))
